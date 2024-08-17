96b6c643c0cfb942ded7322b02b410eb17fecbab
SAHIL SHARMA <118820911+sahildando@users.noreply.github.com>
2024-08-18 03:14:47 +0530

    Implemet auth, appwrite and sentry

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -11,13 +11,13 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
         <div className="profile">
           <div className="profile-img">
             <span className="text-5xl font-bold text-blue-500">
-              {user.firstName[0]}
+              {user.name[0]}
             </span>
           </div>
 
           <div className="profile-details">
             <h1 className="profile-name">
-              {user.firstName} {user.lastName}
+              {user.name} 
             </h1>
             <p className="profile-email">
               {user.email}
@@ -27,7 +27,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
       </section>
       <section className="banks">
         <div className="flex w-full justify-between">
-          <h2 className="header-2">My Banks
+          <h2 className="header-2">My Banks</h2>
             <Link href="/" className="flex gap-2">
               <Image
                 src="/icons/plus.svg"
@@ -41,7 +41,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
                Add Bank
               </h2>
             </Link>
-          </h2>
+          
         </div>
 
         {banks?.length > 0 && (
@@ -52,7 +52,8 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               key={banks[0].$id}
               account={banks[0]}
               userName={`${user.firstName} ${user.lastName}`}
-              showBalance={false}/>
+              showBalance={false}
+              />
 
             </div>
             {banks[1] &&(
--- types/index.d.ts
+++ types/index.d.ts
@@ -26,6 +26,7 @@ declare type LoginUser = {
 };
 
 declare type User = {
+  [x: string]: any;
   $id: string;
   email: string;
   userId: string;
@@ -33,6 +34,7 @@ declare type User = {
   dwollaCustomerId: string;
   firstName: string;
   lastName: string;
+  name: string;
   address1: string;
   city: string;
   state: string;
@@ -210,6 +212,7 @@ declare interface TotlaBalanceBoxProps {
 
 declare interface FooterProps {
   user: User;
+  type?: 'mobile' |  'desktop'
 }
 
 declare interface RightSidebarProps {
